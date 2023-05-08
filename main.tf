3a33f51573e18c9c77aa53e48db46649e2fba144
MOHAMMED NABEEL KM <55877632+nabeelkm-src@users.noreply.github.com>
2023-05-08 14:50:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   profile = "myaws"
-  region  = var.region
+  region  = "us-east-2"
 }
 
 
@@ -14,6 +14,6 @@ resource "aws_s3_bucket" "b" {
   }
 }
 
-variable "region" {
+##variable "region" {
   
-}
+##}
